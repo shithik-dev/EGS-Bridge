fed08ff014c6bd1e4fe2e9308fb32642b24a0c07
shithikasath <shithikasathofficial@gmail.com>
2026-02-01 19:51:15 +0530

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || '/EGS-Bridge/',
   plugins: [react()],
+  base: './',   
 })
